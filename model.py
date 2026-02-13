646d84e8b2d8a6da2e418bab00895ca7c1cd6fe8
Chaithra-023 <you@chaithrachaitu468@gmail.com>
2026-02-13 15:03:09 +0530

    alembic

--- model.py
+++ model.py
@@ -1,17 +1,12 @@
 from db import Base
-from sqlalchemy import Column, Integer, String, ForeignKey, Text, DateTime
-from datetime import datetime
+
 
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
+    name = Column(String)
+    email = Column(String)
     password = Column(String)
-
-class SearchHistory(Base):
-    __tablename__ = "search_history"
-    id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-    query = Column(Text, nullable=False)
-    response = Column(Text, nullable=False)
-    timestamp = Column(DateTime, default=datetime.utcnow)
+    address = Column(String)
+    phone = Column(String)
+    isprime = Column(Boolean)
